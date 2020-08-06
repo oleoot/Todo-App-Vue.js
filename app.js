215fbbbe1009e3ea87dac607dcd3d324a28755b6
oleoot <oleoot28@gmail.com>
2020-08-06 15:45:26 +0300

    Handling input functionality

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const app = new Vue({
     el: '#app',
     data: {
-        title: 'Hello coding garden'
+        title: 'Hello coding garden',
+        newTodo: ''
+    },
+    methods: {
+        addTodo() {
+            console.log("Form submitted")
+        }
     }
 })
